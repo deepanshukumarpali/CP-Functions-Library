4ae55a807809c40afbce9dc8b90094631c57b2e8
Deepanshu Kumar Pali <deepanshukumarpali@Deepanshus-MacBook-Air.local>
2020-08-28 20:02:48 +0530

    function for PrimeFactors

---
+++ python3/prime factors.py
@@ -0,0 +1,25 @@
+from math import *
+
+def PrimeFactors(n):
+    factors=[]
+    while(n%2==0):
+        factors.append(2)
+        n//=2
+    for i in range(3,int(sqrt(n))+1,2):
+        while(n%i==0):
+            factors.append(i)
+            n//=i
+    if(n>1):
+        factors.append(n)
+
+    return factors
+
+
+# Example Test Run
+
+"""
+
+n=30
+print(PrimeFactors(n))
+
+"""
